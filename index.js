cb5d10fd45bbecc371942615a86e9891728dae1b
iamabhishektiwari <abhishek.tiwari.cer15@itbhu.ac.in>
2019-09-02 16:31:08 +0530

    Added database, get and post elements

--- index.js
+++ index.js
@@ -3,27 +3,8 @@ var bodyParser = require('body-parser');
 var sqlite = require('sqlite3');
 
 var db = new sqlite.Database('database.db');
-var quotes = [
-    {
-        id: 1,
-        quote: "The best is yet to come",
-        author: "Unknown",
-        year: 2000
-    },
-    {
-        id: 2,
-        quote: "This is a quote",
-        author: "First Last",
-        year: 1930
-    },
-    {
-        id: 3,
-        quote: "This is another quote",
-        author: "First2 Last2",
-        year: 1910
-    }
-    ];
 // Creation of table and adding initial entries
+// uncomment it when running first time
 // #############################################
 // db.serialize(function () {
 //     db.run("create table quotes(id number primary key, quote varchar(255),author varchar(255),year number)");
@@ -42,33 +23,77 @@ var quotes = [
 //         }        
 //     })
 // })
+//db.close();
 //############################################
-db.close();
+
 
 app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 port = 3000;
 
+
 app.get('/',function (request,response) {
-    response.send("Hello!");
+    response.send("Hello and Welcome to world of Quotes!");
 });
 
-app.get('/quotes',function (request,response) {
-    console.log("list of all quotes");
-    if(request.query.year){
-        response.send(request.query.year)
+app.get('/quotes', function(req, res){
+    if(req.query.year){
+        db.all('SELECT * FROM quotes WHERE year = ?', [req.query.year], function(err, rows){
+            if(err){
+                res.send(err.message);
+            }
+            else{
+                console.log("Return a list of quotes from the year: " + req.query.year);
+                res.json(rows);
+            }
+        });
     }
     else{
-        response.json(quotes);
+        db.all('SELECT * FROM quotes', function processRows(err, rows){
+            if(err){
+                res.send(err.message);
+            }
+            else{
+                for( var i = 0; i < rows.length; i++){
+                    console.log(rows[i].quote);
+                }
+                res.json(rows);
+            }
+        });
     }
+
+    // When using json data
+    // if(request.query.year){
+    //     response.send(request.query.year)
+    // }
+    // else{
+    //     response.json(quotes);
+    // }
 });
 app.get('/quotes/:id',function (request,response) {
-    response.send("returning quotes with id: "+request.params.id);
+    db.serialize(function () {
+        db.all("select * from quotes where id=?",request.params.id,function (err,row) {
+            if (err) {
+                response.send("No quotes available");
+            } else {
+                response.json(row)               
+            } 
+        });
+    })
 })
 
 app.post('/quotes', function (request,response) {
     console.log("enter the quote"+request.body.quote)
-    response.json(request.body);
+    db.run("Insert into quotes values(?,?,?,?)",[request.body.id,request.body.quote,request.body.author,request.body.year],function (err) {
+        if(err){
+            console.log("error occured!");
+            response.send("error occured!"+err);
+        }
+        else{
+            console.log("Success!");
+            response.send("Success!");
+        }
+    })
 })
 
 app.listen(port,function () {
